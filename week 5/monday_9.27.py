d7e562f6ec7a73d9ce6e3a34f33ca5350497e375
raphael-khan <raphaelkhan@yahoo.com>
2021-09-28 19:53:06 -0400

    notes_9.27

---
+++ week 5/monday_9.27.py
@@ -0,0 +1,17 @@
+x = 5
+print(x)
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
+
+for x in range(0, 10, 2):
+    print(x)
+# output: 0, 2, 4, 6, 8
+for x in range(5, 1, -3):
+    print(x)
+# output: 5, 2
+
+for i in 'rapahel':
+    print(i)
