dc0429b7d506ea66650695a24a59c6e22c8567f5
raphael-khan <raphaelkhan@yahoo.com>
2021-10-03 13:46:15 -0400

    10.3

---
+++ week 5/funtions_basic_1.py
@@ -0,0 +1,86 @@
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+
+def number_of_military_branches():
+    return 5
+print(number_of_military_branches())
+
+def number_of_books_on_hold():
+    return 5 
+    return 10
+print(number_of_books_on_hold())
+
+def number_of_fingers():
+    return 5
+    print(10)
+print(number_of_fingers())
+
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+
+7
+def concatenate(b,c):
+    return str(b)+str(c)
+print(concatenate(2,5))
+
+def concatenate(b,c):
+    return b + c
+print(concatenate(2,5))
+
+def number_of_oceans_or_fingers_or_continents():
+    b = 9
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(number_of_oceans_or_fingers_or_continents())
+
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+print(b)
+foobar()
+print(b)
+
+#13
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    # return b
+print(b)
+b=foobar()
+print(b)
+
+#14
+def foo():
+    print(1)
+    bar()
+    print(2)
+def bar():
+    print(3)
+foo()
+
+#15
+def foo():
+    print(1)
+    x = bar()
+    # print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
+
+
+
+
+
