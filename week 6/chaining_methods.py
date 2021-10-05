3dd0ddc8217df78600cb12a7a3f4847fc00cd777
raphael-khan <raphaelkhan@yahoo.com>
2021-10-04 22:57:41 -0400

    10.4

---
+++ week 6/chaining_methods.py
@@ -0,0 +1,43 @@
+class User:
+    bank_name = "First National Dojo"
+    def __init__(self, name, email_address):
+        self.name = name
+        self.email = email_address
+        self.account_balance = 0
+
+    def make_deposit(self,amount):
+        self.account_balance += amount
+        return  self 
+    
+    
+    def withdrawl(self, amount):
+        self.account_balance -= amount
+        return  self
+    
+    def display_user_balance(self):
+        print(f"my name is {self.name} and my account balance is {self.account_balance}")
+    
+    def transfer_money(self,transferring_to, amount):
+        transferring_to.account_balance += amount
+        self.account_balance -= amount
+        print({f"{self.name} transferred {amount} to {transferring_to.name}"})
+
+
+raphael = User('rapahel', 'hello@email.com')
+raphael.make_deposit(500).make_deposit(500).withdrawl(100)
+raphael.display_user_balance()
+# print(raphael.account_balance)
+
+# jenny = User('jenny', 'itsmejen@email.com')
+# jenny.make_deposit(100)
+# print(raphael.account_balance)
+# print(jenny.account_balance)
+
+# jenny.transfer_money(raphael,25)
+# print(raphael.account_balance)
+# print(jenny.account_balance)
+
+
+
+
+
