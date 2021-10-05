94220feedb192b58f557cd01a32b32d025ae4c2c
raphael-khan <raphaelkhan@yahoo.com>
2021-10-04 22:43:55 -0400

    10.4

---
+++ week 6/notes_10.4.py
@@ -0,0 +1,12 @@
+class Employee:
+    def __init__(self, firstName, lastName, employeeId, email, hireDate):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.employeeId = employeeId
+        self.email = email
+        self. hireDate = hireDate
+
+        def welcomeEmployee(self):
+            print(f"welcome to our company {self.firstname} {self.lastName}")
+
+alice = Employee()
