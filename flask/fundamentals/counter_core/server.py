36427b9dc129aa579820b10c69e89c92aeea10c6
raphael-khan <raphaelkhan@yahoo.com>
2021-10-17 21:57:40 -0400

    10.17

--- flask/fundamentals/counter_core/server.py
+++ flask/fundamentals/counter_core/server.py
@@ -3,14 +3,18 @@ app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe'
 @app.route('/')
 def visits():
+    counter = session['counter']
     if 'counter' in session:
         session['counter'] = session.get('counter') + 1
+        return render_template('index.html', counter=counter)
     else:
         session['counter'] = 1
-    return "Total Visits: {}" .format(session.get('counter'))
+    return render_template('index.html')
+
 @app.route('/destroy_session')
 def destroy_session():
     session.pop('counter', None)
     return redirect('/')
+
 if __name__ == '__main__':
     app.run(debug=True)
