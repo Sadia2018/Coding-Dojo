ac9d40e9d396e550a1636316371d0df25f3cc901
raphael-khan <raphaelkhan@yahoo.com>
2021-11-02 22:02:42 -0400

    11.2

---
+++ flask_mysql/dojo_survey_practice/app/controllers/controller.py
@@ -0,0 +1,17 @@
+from app import app
+from flask import Flask, render_template, redirect, request, flash, session
+
+from app.models.dojo import Dojo
+
+#----------- main landing page. 
+@app.route('/')
+def index():
+    return render_template('survey_page.html')
+
+#----------- route that validates teh form data
+@app.route('/process', methods=['POST'])
+def create_dojo():
+    if not Dojo.validate_dojo(request.form):
+        return redirect('/')
+    Dojo.save(request.form)
+    return redirect('/dojos')
---
+++ flask_mysql/dojo_survey_practice/app/models/dojo.py
@@ -0,0 +1,42 @@
+from flask.app import Flask
+from flask.helpers import flash
+from app.config.mysqlconnection import connectToMySQL
+
+class Dojo:
+    db_name = 'dojo_survey_schema'
+    result = connectToMySQL('dojo_survey_schema')
+    def __init__(self , data):
+        self.id = data['id']
+        self.name = data['name']
+        self.location = data['location']
+        self.language = data['language']
+        self.comment = data['comment']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+#---- class method to retrive all dojos from database
+@classmethod
+def get_all(cls):
+    query = 'SELECT * FROM dojos;'
+    results_from_db = connectToMySQL(cls.db_name).query_db(query)
+
+    all_dojos = []
+    for row in results_from_db:
+        all_dojos.append(cls(row))
+    print(all_dojos)
+    return all_dojos
+
+#---- static method to validate form data 
+@staticmethod
+def validate_dojo(dojo):
+    is_valid = True
+    if len(dojo['name']) < 3:
+        flash('Name must be at least 3 characters long')
+        is_valid = False
+    if dojo['location'] != 'San Jose , CA' or 'Arlington, VA':
+        flash('Must select a location')
+        is_valid = False
+    if dojo['language'] != 'Python' or 'Javascript':
+        flash('Must pick a programming langauge to learn')
+        is_valid = False
+    return is_valid
