c78f30f75b3f94edda4346624899fd5630a4470d
raphael-khan <raphaelkhan@yahoo.com>
2021-11-03 19:29:39 -0400

    11.3

--- flask_mysql/dojo_survey_practice/app/models/dojo.py
+++ flask_mysql/dojo_survey_practice/app/models/dojo.py
@@ -1,5 +1,4 @@
-from flask.app import Flask
-from flask.helpers import flash
+from flask import flash
 from app.config.mysqlconnection import connectToMySQL
 
 class Dojo:
@@ -15,28 +14,28 @@ class Dojo:
         self.updated_at = data['updated_at']
 
 #---- class method to retrive all dojos from database
-@classmethod
-def get_all(cls):
-    query = 'SELECT * FROM dojos;'
-    results_from_db = connectToMySQL(cls.db_name).query_db(query)
+    @classmethod
+    def get_all(cls):
+        query = 'SELECT * FROM dojos;'
+        results_from_db = connectToMySQL(cls.db_name).query_db(query)
 
-    all_dojos = []
-    for row in results_from_db:
-        all_dojos.append(cls(row))
-    print(all_dojos)
-    return all_dojos
+        all_dojos = []
+        for row in results_from_db:
+            all_dojos.append(cls(row))
+        print(all_dojos)
+        return all_dojos
 
 #---- static method to validate form data 
-@staticmethod
-def validate_dojo(dojo):
-    is_valid = True
-    if len(dojo['name']) < 3:
-        flash('Name must be at least 3 characters long')
-        is_valid = False
-    if dojo['location'] != 'San Jose , CA' or 'Arlington, VA':
-        flash('Must select a location')
-        is_valid = False
-    if dojo['language'] != 'Python' or 'Javascript':
-        flash('Must pick a programming langauge to learn')
-        is_valid = False
-    return is_valid
+    @staticmethod
+    def validate_dojo(dojo):
+        is_valid = True
+        if len(dojo['name']) < 3:
+            flash('Name must be at least 3 characters long')
+            is_valid = False
+        if dojo['location'] == "":
+            flash('Must select a location')
+            is_valid = False
+        if dojo['language'] == "":
+            flash('Must pick a programming langauge to learn')
+            is_valid = False
+        return is_valid
