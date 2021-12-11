d02dc08f2670c5c6e0cb5d88c1a22ce4cda807cd
raphael-khan <raphaelkhan@yahoo.com>
2021-12-10 21:51:05 -0500

    12.10

--- projects_algos/fronts.js
+++ projects_algos/fronts.js
@@ -1,15 +1,39 @@
-// Add Front
-// Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
-
-class Node {
-    constructor(data){
-        this.data = data
-        this.next = null
+class SLLNode { // Singly-linked list node class
+    constructor(val) {
+        this.value = val; // Holds value for this node
+        this.next = null; // Pointer to next node
     }
 }
 
-class linkedList {
-    constructor(){
-        this.head = null
+class SLL { // The singly-linked list class itself
+    constructor() { // Will start with no nodes
+        this.head = null; // Head points to first node
     }
+
+//ADD FRONT
+
+addFront(value) {
+    var newNode = new SLLNode(value); 
+    newNode.next = this.head; 
+    this.head = newNode; 
+    return this.head; 
+}
+
+//REMOVE
+
+removeFront() {
+    if (this.head == null) {
+        return this.head;
+    }
+    var removedNode = this.head; 
+    this.head = removedNode.next; 
+    return this.head;
+}
+
+// Return the value at the front (head) of the list
+front() {
+if (this.head == null) {
+    return null;
+} else {
+    return this.head.value;
 }
